83fbe683e1f782d1548cb43fac34d932878b9914
Le Ngoc Hieu <lengochieutlu11112003.com>
2024-07-25 22:20:20 +0700

    update - tower graphics

--- code/settings.py
+++ code/settings.py
@@ -8,7 +8,7 @@ TILE_SIZE = 64
 # overlay positions
 OVERLAY_POSITIONS = {
     'tool': (40, SCREEN_HEIGHT - 15),
-    'defense base': (70, SCREEN_HEIGHT - 5)
+    'defense base': (70, SCREEN_HEIGHT - 70)
 }
 
 PLAYER_TOOL_OFFSET = {
