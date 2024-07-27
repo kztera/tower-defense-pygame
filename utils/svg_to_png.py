4ba0471f41d24a3ac0d0cccbdf26f422b28d28a3
kztera <116504460+kztera@users.noreply.github.com>
2024-07-27 11:38:39 +0700

    chore: Update SVG to PNG conversion script

--- utils/svg_to_png.py
+++ utils/svg_to_png.py
@@ -1,31 +1,50 @@
-import cairosvg
 import os
 
+# Cài đặt GTK3-Runtime Win64 để sử dụng cairosvg trên windows
+# gtkbin = r"C:\Program Files\GTK3-Runtime Win64\bin"
+# add_dll_dir = getattr(os, "add_dll_directory", None)
+# if callable(add_dll_dir):
+#     add_dll_dir(gtkbin)
+# else:
+#     os.environ["PATH"] = os.pathsep.join((gtkbin, os.environ["PATH"]))
 
-def convert_svg_to_png(directory_path, width, height):
-    for root, dirs, files in os.walk(directory_path):
+import cairosvg
+from PIL import Image
+
+
+def convert_svg_to_png(input_path, output_path, max_size=128 * 4):
+    cairosvg.svg2png(
+        url=input_path,
+        write_to=output_path,
+        output_width=max_size,
+        output_height=max_size,
+    )
+
+    with Image.open(output_path) as img:
+        img.thumbnail((max_size, max_size), Image.Resampling.LANCZOS)
+        img.save(output_path, "PNG")
+
+
+def process_directory(input_dir, output_dir):
+    for root, dirs, files in os.walk(input_dir):
         for file in files:
             if file.lower().endswith(".svg"):
-                svg_path = os.path.join(root, file)
-                png_path = os.path.splitext(svg_path)[0] + ".png"
+                input_path = os.path.join(root, file)
+                relative_path = os.path.relpath(input_path, input_dir)
+                output_path = os.path.join(
+                    output_dir, os.path.splitext(relative_path)[0] + ".png"
+                )
+
+                os.makedirs(os.path.dirname(output_path), exist_ok=True)
 
                 try:
-                    cairosvg.svg2png(
-                        url=svg_path,
-                        write_to=png_path,
-                        output_width=width,
-                        output_height=height,
-                    )
-                    print(f"Converted: {svg_path} to {png_path}")
-
-                    os.remove(svg_path)
-                    print(f"Removed original SVG: {svg_path}")
+                    convert_svg_to_png(input_path, output_path)
+                    print(f"Converted: {input_path} -> {output_path}")
                 except Exception as e:
-                    print(f"Error processing {svg_path}: {str(e)}")
+                    print(f"Error converting {input_path}: {str(e)}")
 
 
+# Sử dụng script
 input_directory = "../graphics/towers"
-desired_width = 128
-desired_height = 128
-
-convert_svg_to_png(input_directory, desired_width, desired_height)
+output_directory = "../png"
+process_directory(input_directory, output_directory)
